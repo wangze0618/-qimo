a4b4c48c0e7c16411bc435baf280ef5a48ce01e3
wangze0618 <2867273828@qq.com>
2021-06-21 22:52:19 +0800

    banner 100%

--- js/indexBanner.js
+++ js/indexBanner.js
@@ -1,4 +1,5 @@
 window.onload = function () {
+    // 吸顶效果
     window.onscroll = function () {
         var scrolltop = window.pageYOffset || document.body.scrollTop || document.documentElement.scrollTop;
         var navitems = document.getElementById('navitems');
@@ -10,10 +11,6 @@ window.onload = function () {
             navitems.className = 'navitems fl'
         }
     }
-    var ftl = document.getElementsByClassName('footList')[0];
-    var lis = ftl.getElementsByTagName('li');
-
-
 
     var vm = new Vue({
         el: '.focus',
@@ -25,22 +22,25 @@ window.onload = function () {
                 "../upload/4.jpg",
                 "../upload/5.jpg"],
             index: 0,
-            that: this,
+            indexx: 0,
         },
         mounted: function () {
-            this.autoPlay();//需要触发的函数
+            //需要加载时触发的函数
+            this.autoPlay()
+            // this.isActive()
         },
+
         methods: {
-            // 鼠标单击切换上一张
             prev() {
+                // 鼠标单击切换上一张
                 if (this.index > 0) {
                     this.index--
                 } else {
                     this.index = this.imgList.length - 1
                 }
             },
-            // 鼠标单击切换下一张
             next() {
+                // 鼠标单击切换下一张
                 if (this.index < this.imgList.length - 1) {
                     this.index++
                 } else {
@@ -56,7 +56,6 @@ window.onload = function () {
                         vm.index = 0
                     }
                 }
-
                 timer = setInterval(auto, 3500)
             },
             stopPlay() {
@@ -67,8 +66,12 @@ window.onload = function () {
                 // 鼠标移出重新播放
                 // 怎么在这里调用autoPlay()方法里的auto()函数
                 this.$options.methods.autoPlay()
-            }
+            },
+            // isActive() {
+            //     if (index == vm.index) {
+            //         console.log('hello');
+            //     }
+            // }
         }
-    }
-    )
+    })
 }
